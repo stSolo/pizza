02ddf376520f705fb87403af861670df5a9b0cd4
Stanislav Solovyev <st.solp@gmail.com>
2020-06-19 22:28:27 +0300

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Media from 'reactstrap';
+import { Media } from 'reactstrap';
 
 function Menu(){
     const [dishes, setDishes] = React.useState([
@@ -43,7 +43,21 @@ function Menu(){
 
 
 
-    const menu;
+    const menu = dishes.map(dish => {
+        return (
+            <div key = {dish.id} className = 'col-12 mt-5'>
+                <Media tag = 'li'>
+                    <Media left middle>
+                        <Media object src = {dish.image} alt = {dish.name}/>
+                    </Media>
+                    <Media body className = 'ml-5'>
+                        <Media heading>{dish.name}</Media>
+                        <p>{dish.description}</p>
+                    </Media>
+                </Media>
+            </div>
+        );
+    });
 
     return (
         <div className = 'container'>
