b70061cf562e6a45a3325b37856dcaf64139f9ef
Stanislav Solovyev <st.solp@gmail.com>
2020-06-21 08:05:16 +0300

    Update MenuComponent.jsx
    Add onClick method, rendering selevted Dish

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -10,10 +10,26 @@ import {
 
 function Menu(props){
     
+    const [selectedDish, setSelectedDish] = React.useState(null);
+
+    function renderDish(dish){
+        if(dish){
+            return(
+                <Card>
+                    <CardImg width = '100%' src = {dish.image} alt = {dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+    }
+
     const menu = props.dishes.map(dish => {
         return (
             <div key = {dish.id} className = 'col-12 col-md-5 m-1'>
-                <Card>
+                <Card onClick = {() => setSelectedDish({...dish})}>
                     <CardImg width = '100%' src = {dish.image} alt = {dish.name}/>
                     <CardImgOverlay body className = 'ml-5'>
                         <CardTitle>{dish.name}</CardTitle>
@@ -27,6 +43,9 @@ function Menu(props){
         <div className = 'container'>
             <div className = 'row'>
                 {menu}
+            </div>
+            <div className = 'row'>
+                {renderDish(selectedDish)}
             </div>
         </div>
     );
