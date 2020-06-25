a2b5181239c61ad006fff66a83efc7339e105c9c
Stanislav Solovyev <st.solp@gmail.com>
2020-06-25 11:22:27 +0300

    Update MenuComponent.jsx

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -4,7 +4,10 @@ import {
     CardImg, 
     CardImgOverlay, 
     CardTitle,
+    Breadcrumb, 
+    BreadcrumbItem,
 } from 'reactstrap';
+import { Link } from '@reach/router';
 import DishDetail from './DishdetailComponent';
 
 function Menu(props){
@@ -14,11 +17,13 @@ function Menu(props){
     const menu = props.dishes.map(dish => {
         return (
             <div key = {dish.id} className = 'col-12 col-md-5 m-1'>
-                <Card onClick = {() => setSelectedDish({...dish})}>
-                    <CardImg width = '100%' src = {dish.image} alt = {dish.name}/>
-                    <CardImgOverlay body className = 'ml-5'>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
+                <Card>
+                    <Link to={`/menu/${dish.id}`} >
+                        <CardImg width = '100%' src = {dish.image} alt = {dish.name}/>
+                        <CardImgOverlay body className = 'ml-5'>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Link>
                 </Card>
             </div>
         );
@@ -26,12 +31,19 @@ function Menu(props){
 
     return (
         <div className = 'container'>
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>                
+            </div>
             <div className = 'row'>
                 {menu}
             </div>
-            <DishDetail 
-            selectedDish = {selectedDish}
-            />
         </div>
     );
 }
