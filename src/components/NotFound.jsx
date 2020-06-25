70c92ef73e4b4afd64b35f30942b37b648b1893a
Stanislav Solovyev <st.solp@gmail.com>
2020-06-25 08:07:42 +0300

    Create NotFound.jsx

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound(){
+    return (
+        <div className = 'container'>
+            Not Found
+        </div>
+    );
+};
+
+export default NotFound;
