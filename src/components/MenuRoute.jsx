6eaf613dc6f28dc9da7c54ebc5bbb812a77b9244
Stanislav Solovyev <st.solp@gmail.com>
2020-06-25 13:33:36 +0300

    Create MenuRoute.jsx

---
+++ src/components/MenuRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuRoute(props){
+    return(
+        <React.Fragment>
+            {props.children}
+        </React.Fragment>
+    );
+}
+
+export default MenuRoute;
