d64916f82bbb5cc09f865bfa8059818965d7dd56
Stanislav Solovyev <st.solp@gmail.com>
2020-06-25 08:08:02 +0300

    Add not found and reach router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import NotFound from './components/NotFound';
+import { Router } from '@reach/router';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
@@ -8,7 +10,10 @@ function App() {
 
   return (
     <div className="App">
-      <Main />
+    <Router>
+      <Main path = '/' />
+      <NotFound default />
+    </Router>
     </div>
   );
 }
