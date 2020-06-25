cf45243351cd4edcd313f24289b18defaab6991f
Stanislav Solovyev <st.solp@gmail.com>
2020-06-25 09:28:57 +0300

    Update MainComponent.jsx

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -3,13 +3,19 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import NotFound from './NotFound';
 import { Router } from '@reach/router';
-import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 function Main() {
     const dishes = DISHES;
+    const leaders = LEADERS;
+    const promotions = PROMOTIONS;
+    const comments = COMMENTS;
         
     const [selectedDish, setSeelctedDish] = React.useState(null);
 
@@ -21,8 +27,13 @@ function Main() {
       <div>
         <Header />
         <Router>
-          <Home path = '/' />
+          <Home path = '/' 
+            dish={dishes.filter((dish) => dish.featured)[0]}
+            promotion={promotions.filter((promo) => promo.featured)[0]}
+            leader={leaders.filter((leader) => leader.featured)[0]}
+          />
           <Menu path = 'menu' dishes={dishes} onClick={(dishId) => onDishSelect(dishId)} />
+          <Contact path = 'contactus' />
           <NotFound default />
         </Router>
         <Footer />
