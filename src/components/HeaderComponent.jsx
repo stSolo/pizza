c8a0937fab799f6246e975fb7e10af7bb84dcf37
Stanislav Solovyev <st.solp@gmail.com>
2020-06-28 08:28:54 +0300

    Update HeaderComponent.jsx

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label,
+} from 'reactstrap';
 import { Link } from '@reach/router';
 
 function Header() {
       const [isNavOpen, setNavOpen] = React.useState(false);
+      const [isModalOpen, setModalOpen] = React.useState(false);
+
+      const username = React.useRef();
+      const password = React.useRef();
+      const remember = React.useRef();
 
       function toggleNav() {
         setNavOpen(!isNavOpen);
       }
+
+      function toggleModal() {
+        setModalOpen(!isModalOpen);
+      }
+
+      function handleLogin(event) {
+        toggleModal();
+        event.preventDefault();
+        console.log(username);
+        alert("Username: " + username.current.value + " Password: " + password.current.value
+            + " Remember: " + remember.current.checked);
+
+        }
 
       const isActive = ({isCurrent}) => {
           return isCurrent?{className: 'nav-link active'}:null
@@ -35,6 +56,11 @@ function Header() {
                             </NavItem>
                             </Nav>
                         </Collapse>
+                        <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                        </Nav>
                     </div>
                 </Navbar>
                 <Jumbotron>
@@ -47,6 +73,31 @@ function Header() {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={isModalOpen} toggle={toggleModal}>
+                    <ModalHeader toggle={toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={username} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={password}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={remember}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
 }
