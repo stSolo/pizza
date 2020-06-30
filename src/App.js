c38a966c135b407072f80056ccf4286ff3dbdbaf
Stanislav Solovyev <st.solp@gmail.com>
2020-06-30 16:41:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Main from './components/MainComponent';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 function App() {
 
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Provider store = {store}>
+      <div className="App">
+        <Main />
+      </div>
+    </Provider>
   );
 }
 
